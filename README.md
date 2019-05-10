1f3c2f689c1842018e26fd9a41fbfd3ebcee4bbb
Ubuntu <ubuntu@ip-172-31-51-217.ec2.internal>
2019-05-10 02:00:52 +0200

    First

---
+++ README.md
@@ -0,0 +1,71 @@
+# pastebinWhisper
+> Get the newest emails and possible passwords from pastebin (Python3 based)
+
+
+## Instalation
+
+Install python3 and pip3
+
+<pre> sudo apt install python3 </pre>
+<pre> sudo apt install python3-pip</pre>
+
+Install the dependencies via pip:
+
+<pre> pip3 install -r requirements.txt </pre>
+
+#### Important
+
+Change DB Config (mysql)
+<pre>
+host="xxxx.xxxx.us-east-1.rds.amazonaws.com"
+port=3306
+dbname="xxxxx"
+user="xxxxx"
+password="xxxxxx"
+</pre>
+
+Add "Example schema table" in your database
+<pre>
+CREATE TABLE `pastebin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(250),
+  `line` varchar(250),
+  `url` varchar(250),
+  `ref` varchar(45),
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+</pre>
+
+
+## Usage
+<pre>usage: python3 pastebinWhisper.py  </pre>
+
+
+## Example
+<pre>
+python3 pastebinWhisper.py
+
+
+ ____           _       ____  _    __        ___     _                     
+|  _ \ __ _ ___| |_ ___| __ )(_)_ _\ \      / / |__ (_)___ _ __   ___ _ __
+| |_) / _` / __| __/ _ \  _ \| | '_ \ \ /\ / /| '_ \| / __| '_ \ / _ \ '__|
+|  __/ (_| \__ \ ||  __/ |_) | | | | \ V  V / | | | | \__ \ |_) |  __/ |   
+|_|   \__,_|___/\__\___|____/|_|_| |_|\_/\_/  |_| |_|_|___/ .__/ \___|_|   
+                                                          |_|              
+
+-------
+https://scrape.pastebin.com/api_scrape_item.php?i=XXXXXX
+XXXXXX@localhost.loca
+XXXXXXX@localhost.localdomain
+-------
+https://scrape.pastebin.com/api_scrape_item.php?i=XXXXXX
+XXXXXXX@gmail.com
+XXXXXXX@gmail.com"}],"paginas":2,"rutaVdm":"1,10,81","indexar":true,"documentoId":"CRI-0000695","mostrar":true,"uniqu
+-------
+https://scrape.pastebin.com/api_scrape_item.php?i=XXXXXX
+XXXXXXXX@tree-xxxx-xxxxx.com
+-------
+https://scrape.pastebin.com/api_scrape_item.php?i=XXXXXX
+XXXXXXXX@gmail.com
+XXXXXXXX@gmail.com/25I020T3rw/64545
---
+++ pastebinWhisper.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import bs4, requests,re, sys
+import pymysql
+import datetime
+import time
+import json
+import urllib.parse
+import pyfiglet
+
+
+_headers_get = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+}
+
+#Time API restrictions, 60 sec recommended
+#https://pastebin.com/doc_scraping_api
+time_tag = 60
+
+#### DB Config ####
+host="xxxx.xxxx.us-east-1.rds.amazonaws.com"
+port=3306
+dbname="xxxxx"
+user="xxxxx"
+password="xxxxxx"
+
+#Example schema table
+'''
+CREATE TABLE `pastebin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(250),
+  `line` varchar(250),
+  `url` varchar(250),
+  `ref` varchar(45),
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+'''
+
+def searchEmail(content):
+    regex = r"[\w.%+-]+@(?:[a-z\d-]+\.)+[a-z]{2,4}"
+    email = re.findall(regex, str(content))
+    return email[0]
+
+def searchEmailALine(content):
+    lines = re.sub("<.*?>", " ", content)
+    regex = r"[\w.%+-]+@(?:[a-z\d-]+\.)+[a-z]{2,4}.{0,100}"
+    lines = re.findall(regex, lines)
+    return lines
+
+
+
+def getUrlContent(url):
+    result = requests.get(
+        url,
+        headers=_headers_get
+    )
+    return result.text
+
+
+def getPastebinEmail(conn):
+    result = requests.get(
+        "https://scrape.pastebin.com/api_scraping.php?limit=250",
+        headers=_headers_get
+    )
+    c = result.json()
+    for value in c:
+        ref = value['key']
+        url = value['scrape_url']
+        content = getUrlContent(url)
+        lines = searchEmailALine(content)
+        for line in lines:
+            line_ = re.sub('\s+', ' ', line).strip()
+            email = searchEmail(line_)
+            line = urllib.parse.quote(line_)
+            with conn.cursor() as cur:
+                exist = cur.execute('select email from pastebin where email = "%s" LIMIT 1' % email)
+                if exist==0:
+                    print("-------")
+                    print(url)
+                    print(email)
+                    if line_!=email:
+                        print(line_)
+                    cur.execute("insert into pastebin (email, line, ref, url) values('%s', '%s', '%s', '%s')" % (email,line,ref,url))
+        time.sleep(0.5)
+
+def main():
+    ascii_banner = pyfiglet.figlet_format("PasteBinWhisper")
+    print("\n"+ascii_banner)
+    conn = pymysql.connect(host, user=user,port=port,passwd=password, db=dbname)
+    while True:
+        print(datetime.datetime.now().strftime("%H:%M %d/%m/%y"))
+        # Search
+        getPastebinEmail(conn)
+        conn.commit()
+        time.sleep(time_tag)
+
+if __name__ == "__main__":
+    main()
